244ea9ea1874daa5a0170d395f5e06f99c18f546
alekseno <aleksenocode@gmail.com>
2025-11-05 18:24:49 +0300

    3 november 2025

--- time/les_171.py
+++ time/les_171.py
@@ -2,3 +2,11 @@ class Point:
     color = "red"
     circle = 2
 
+
+    def set_coords(self):
+        print("вызов метода set_coords" + str(self))
+
+pt = Point()
+pt.set_coords()
+Point.set_coords(1)
+
---
+++ time/les_173.py
@@ -0,0 +1,10 @@
+class Point:
+    "Класс с координатами"
+    color = 'red'
+    circle = 2
+
+    def set_coords():
+        print("происходит вызов функции")
+
+
+Point.set_coords()
